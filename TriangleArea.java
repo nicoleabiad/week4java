e679f46e6f9ca60ed014cb182ebb0918234dee39
nicoleabiad <s3488349@student.rmit.edu.au>
2021-05-12 18:58:29 +1000

    Initial triangle area code

--- TriangleArea.java
+++ TriangleArea.java
@@ -8,12 +8,27 @@ public class TriangleArea {
       Triangle triangle2 = new Triangle();
 
       // TODO: Read and set base and height for triangle1 (use setBase() and setHeight())
-      
+      double base1 = scnr.nextDouble();
+      double height1 = scnr.nextDouble();
+      triangle1.setBase(base1);
+      triangle1.setHeight(height1);
+   
       // TODO: Read and set base and height for triangle2 (use setBase() and setHeight())
+      double base2 = scnr.nextDouble();
+      double height2 = scnr.nextDouble();
+      triangle2.setBase(base2);
+      triangle2.setHeight(height2);
       
       // TODO: Determine larger triangle (use getArea())
       
       System.out.println("Triangle with larger area:");
       // TODO: Output larger triangle's info (use printInfo())
+      if(triangle1.getArea() > triangle2.getArea()) {
+         triangle1.printInfo();
+      }
+      else {
+         triangle2.printInfo();
+      }
+      
    }
 }
